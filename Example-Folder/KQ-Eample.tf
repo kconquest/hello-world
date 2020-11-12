961b2edd010ae7fbd50b6c09cdef334d068415e9
kconquest <67344725+kconquest@users.noreply.github.com>
2020-11-12 16:54:39 -0600

    First edit of Example code!  Yay!

--- Example-Folder/KQ-Eample.tf
+++ Example-Folder/KQ-Eample.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-resource "aws_instance" "KQuest-example" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
-}
+#resource "aws_instance" "KQuest-example" {
+#  ami           = "ami-830c94e3"
+#  instance_type = "t2.micro"
+#}
